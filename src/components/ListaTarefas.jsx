cc8e48e14bbb040d97cc1403ae975cfa6307f1fd
vomeajuda <leogugo9@hotmail.com>
2025-03-27 15:23:49 -0300

    basico feito

---
+++ src/components/ListaTarefas.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+function ListaTarefas() {
+    const [tarefas, setTarefas] = useState([]);
+    const [novaTarefa, setNovaTarefa] = useState('');
+
+    const adicionarTarefa = () => {
+        if (novaTarefa.trim() !== '') {
+            setTarefas([...tarefas, novaTarefa]);
+            setNovaTarefa("");
+        }
+    };
+
+    const removerTarefa = (indice) => {
+        setTarefas(tarefas.filter((_, i) => i !== indice));
+    };
+
+    return (
+        <div>
+            <h2>Lista de Tarefas</h2>
+            <input
+                type="text"
+                value={novaTarefa}
+                onChange={(e) => setNovaTarefa(e.target.value)}
+                placeholder='Digite uma nova tarefa'
+            />
+            <button onClick={adicionarTarefa}>Adicionar</button>
+            <ul>
+                {tarefas.map((tarefa, indice) => (
+                    <li key={indice}>
+                        {tarefa} <button onClick={() =>
+                            removerTarefa(indice)}>Remover</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ListaTarefas;
